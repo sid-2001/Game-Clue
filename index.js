87751a185f73774c28c0e8fe2d073c30024ab6a5
Prashant Bansal <67036593+prashant0829@users.noreply.github.com>
2020-10-22 22:13:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var link = document.getElementById("answer-here")
 submit.addEventListener("click",() =>{
     console.log(answer.value)
     console.log(answer.value.toLowerCase());
+    answer.value = ""
     if(verify === answer.value.toLowerCase()){
         console.log("Congratulations")
         link.setAttribute("href","https://www.google.com/")
