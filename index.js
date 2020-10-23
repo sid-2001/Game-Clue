3ccc408d3e2b51b7e38064836d2cd14abc74bb17
Prashant Bansal <prashantbansal500@gmail.com>
2020-10-23 21:49:35 +0530

    changes made

---
+++ index.js
@@ -0,0 +1,40 @@
+var answer = document.getElementById("answer")
+var inputSection = document.getElementById("input-section")
+var submit = document.getElementById("submit")
+var response = document.getElementById("response")
+var winResult = document.getElementById("win-result")
+var looseResult = document.getElementById("loose-result")
+var verify = "github";
+// looseResult.style.display = "none"
+winResult.style.display = "none"
+var link = document.getElementById("answer-here")
+var wrong = document.createElement("div")
+var i = document .createElement("i")
+wrong.appendChild(i)
+i.classList.add("far","fa-times-circle", "fa-4x", "wrong-color")
+
+submit.addEventListener("click",() =>{
+    console.log(answer.value)
+    console.log(answer.value.toLowerCase());
+    
+    if(verify === answer.value.toLowerCase()){
+        console.log("Congratulations")
+        response.innerText = "Congrats You Can Proceed"
+        link.setAttribute("href","https://www.google.com/")
+        inputSection.style.display = "none"
+        winResult.style.display = ""
+        winResult.classList.add("animate")
+    }else{
+        looseResult.style.display = ""
+        response.innerHTML = "<div><h4>Wrong Answer! Please Try Again</h4><i></i></div>"
+        response.appendChild(wrong)
+
+        // response.lastChild.classList.add("far","fa-times-circle", "fa-4x")
+        console.log("Wrong Answer")
+    }
+    answer.value = ""
+
+
+})
+
+
